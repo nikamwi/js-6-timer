e55b1c1fb7efecaad3d2bacbe30d7af5fe24e2e2
nikamwi <114087929+nikamwi@users.noreply.github.com>
2022-12-19 09:49:20 -0800

    dasrulebuli

--- timer.js
+++ timer.js
@@ -1,20 +1,20 @@
-let hour, minute,second;
+let hour, minute,second, step;
 
 function settime(){
     document.getElementById('hour').innerHTML = document.getElementById('sethour').value;
     document.getElementById('minute').innerHTML = document.getElementById('setminute').value;
     document.getElementById('second').innerHTML = document.getElementById('setsecond').value;
-    document.getElementById('settimer').style.display = "none";
+    document.getElementById('settimisdiv').style.display = "none";
     document.getElementById('divi').style.display = "block";
     
 }
 
 
 function start() {
-    hour = document.getElementById('hour').innerHTML;
-    minute = document.getElementById('minute').innerHTML;
-    second = document.getElementById('second').innerHTML;
-
+    let hour = document.getElementById('hour').innerHTML;
+    let minute = document.getElementById('minute').innerHTML;
+    let second = document.getElementById('second').innerHTML;
+    let step = 1000;
 
     hour  *= 1;
     minute  *= 1;
@@ -27,29 +27,44 @@ function start() {
         if (second > 0) {
             second -= 1;
         }
-        else if (second = 0 || minute > 0) {
+        else if (second == 0 && minute > 0) {
             minute -= 1;
             second = 60;
         }
-        else if (second = 0 || minute == 0 || hour > 0) {
+        else if (second == 0 && minute == 0 && hour > 0) {
             hour-= 1;
             minute = 60;
         }
-        else if (second == 0 || minute == 0 || hour == 0) {
-            second == 0;
-            minute == 0;
-            hour == 0;
+        else if (second == 0 && minute == 0 && hour == 0) {
+            clearInterval(intervalId);
+            document.getElementById('settimisdiv').style.display = "block";
+            document.getElementById('divi').style.display = "none";
         }
-        document.getElementById("second").innerHTML = second
-        document.getElementById("minute").innerHTML = minute
-        document.getElementById("hour").innerHTML = hour
-        document.getElementById("start").style.display = "none"
-        document.getElementById("paus").style.display = "block"
-    }, 1000);
+        document.getElementById("second").innerHTML = second;
+        document.getElementById("minute").innerHTML = minute;
+        document.getElementById("hour").innerHTML = hour;
+        document.getElementById("start").style.display = "none";
+        document.getElementById("paus").style.display = "block";
+    }, step);
 }
 
 function round() {
-    const round = document.createElement('div');
-    round.innerText = `${hPassed}:${mPassed}:${sPassed}`;
-    round.append(round);
+    let roundebi = document.createElement('rounds');
+    roundebi.innerHTML = document.getElementById("timer").innerHTML;
+    document.getElementById("rounds").appendChild(roundebi);
+}
+
+function pausa() {
+    clearInterval(intervalId)
+    document.getElementById("start").style.display = "block";
+    document.getElementById("paus").style.display = "none";
+}
+
+function reset() {
+    clearInterval(intervalId)
+    document.getElementById('settimisdiv').style.display = "block";
+    document.getElementById('divi').style.display = "none";
+    document.getElementById("start").style.display = "block";
+    document.getElementById("paus").style.display = "none";
+    document.getElementById("rounds").innerHTML = "";
 }
